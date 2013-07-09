c2e9f15c949ef89f116ebf26126796df56f7a3cb
Angello Pozo <angellopozo@gmail.com>
2013-07-08 23:47:18 -0700

    sneindg the index.html file

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
-
+var fs = require('fs')
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  // response.send('Hello World!');
+  var out = fs.readFileSync('index.html');
+  response.send(out.toString())
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+app
+.listen(port, function() {
   console.log("Listening on " + port);
 });
